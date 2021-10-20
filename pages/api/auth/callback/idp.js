f672610f748e58627236c8c8a09bd1283ba798ea
Fede Varayoud <varayoudfederico@gmail.com>
2021-10-20 15:59:24 -0300

    Changes

--- pages/api/auth/callback/idp.js
+++ pages/api/auth/callback/idp.js
@@ -1,4 +1,13 @@
 export default function handler(req, res) {
   console.log(req.query);
-  res.status(200).json({ data: req.query });
+  if (req.query.code) {
+    res.writeHead(302, {
+      Location:
+        "https://idpsesiont.telecom.com.ar/openam/oauth2/realms/convergente/access_token",
+      //add other headers here...
+    });
+    res.end();
+  } else {
+    res.status(200).json({ data: req.query });
+  }
 }

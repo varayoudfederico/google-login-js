0f9918ddb0fbf1d8b91e7f49ab010279dfa808e0
Fede Varayoud <varayoudfederico@gmail.com>
2021-10-20 14:48:19 -0300

    Config changes

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -3,25 +3,25 @@ import GoogleProvider from "next-auth/providers/google";
 // import Providers from "next-auth/providers";
 
 export default NextAuth({
-  // callbacks: {
-  //   async jwt(token, account) {
-  //     console.log("token cb", token);
-  //     console.log("account cb", account);
-  //     // Persist the OAuth access_token to the token right after signin
-  //     if (account?.accessToken) {
-  //       token.accessToken = account.accessToken;
-  //     }
-  //     return token;
-  //   },
-  //   async session(session, token, user) {
-  //     console.log("session se", session);
-  //     console.log("token se", token);
-  //     console.log("user se", user);
-  //     // Send properties to the client, like an access_token from a provider.
-  //     session.user.id = token.sub;
-  //     return session;
-  //   },
-  // },
+  callbacks: {
+    async jwt(token, account) {
+      console.log("token cb", token);
+      console.log("account cb", account);
+      // Persist the OAuth access_token to the token right after signin
+      if (account?.accessToken) {
+        token.accessToken = account.accessToken;
+      }
+      return token;
+    },
+    async session(session, token, user) {
+      console.log("session se", session);
+      console.log("token se", token);
+      console.log("user se", user);
+      // Send properties to the client, like an access_token from a provider.
+      // session.user.id = token.sub;
+      return session;
+    },
+  },
   // jwt: {
   //   encryption: false,
   // },
@@ -59,8 +59,8 @@ export default NextAuth({
       wellKnown:
         "https://idpsesiont.telecom.com.ar/openam/oauth2/realms/convergente/.well-known/openid-configuration",
       authorization: { params: { scope: "openid profile" } },
-      idToken: true,
-      checks: ["both"],
+      // idToken: true,
+      // checks: ["none"],
       clientId: process.env.IDP_CLIENT_ID,
       clientSecret: process.env.IDP_CLIENT_SECRET,
       // profile(profile) {

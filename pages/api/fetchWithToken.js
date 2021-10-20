1e00c41c8724c67a704f4e32407ae2230259810e
Fede Varayoud <varayoudfederico@gmail.com>
2021-10-20 14:34:04 -0300

    Fix imports

--- pages/api/fetchWithToken.js
+++ pages/api/fetchWithToken.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { getSession } from "next-auth/client";
+// import { getSession } from "next-auth/client";
 import { getToken } from "next-auth/jwt";
 
 const secret = process.env.SECRET;

2d280b3b50ac2cf0f50996afc23ee27a36e661a5
Fede Varayoud <varayoudfederico@gmail.com>
2021-10-21 16:39:25 -0300

    Add console logs

--- pages/success.js
+++ pages/success.js
@@ -11,16 +11,18 @@ const Success = () => {
 
   useEffect(() => {
     const id_token = cookieCutter.get("idp_id_token");
+    const refresh_token = cookieCutter.get("idp_refresh_token");
+    const access_token = cookieCutter.get("idp_access_token");
+
+    console.log("ID TOKEN: ", id_token);
+    console.log("REFRESH TOKEN: ", refresh_token);
+    console.log("ACCESS TOKEN: ", access_token);
 
     if (id_token) {
       console.log("token from cookie", id_token);
       setDecodedData(jwt_decode(id_token));
     }
   }, []);
-
-  // useEffect(() => {
-  //   if (id_token) setDecodedData(jwt_decode(id_token));
-  // }, [id_token]);
 
   useEffect(() => {
     console.log("Decoded JWT: ", decodedData);
@@ -48,15 +50,6 @@ const Success = () => {
 
       <main className="p-12 max-w-6xl flex flex-col">
         <h1 className="text-2xl font-bold pb-4">Success</h1>
-        {/* <p>
-          <span className="font-bold">ID Token:</span> {id_token}
-        </p>
-        <p>
-          <span className="font-bold">Access Token:</span> {access_token}
-        </p>
-        <p>
-          <span className="font-bold">Refresh Token:</span> {refresh_token}
-        </p> */}
         <p>
           <span className="font-bold">Sub:</span> {decodedData?.sub}
         </p>
@@ -67,42 +60,6 @@ const Success = () => {
     </>
   );
 };
-
-export async function getServerSideProps(context) {
-  try {
-    if (
-      context.query.id_token &&
-      context.query.access_token &&
-      context.query.refresh_token
-    ) {
-      const id_token = context.query.id_token;
-      const access_token = context.query.access_token;
-      const refresh_token = context.query.refresh_token;
-      return {
-        props: {
-          id_token: id_token,
-          access_token: access_token,
-          refresh_token: refresh_token,
-        },
-      };
-    } else {
-      return {
-        props: {
-          id_token: null,
-          access_token: null,
-          refresh_token: null,
-        },
-      };
-    }
-  } catch (error) {
-    console.error(error);
-    return {
-      props: {
-        err: "not_found",
-      },
-    };
-  }
-}
 
 // export async function getServerSideProps(context) {
 //   try {

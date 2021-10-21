6ab9115a3f7c1f17efdb1530eb030eb11fe41555
Fede Varayoud <varayoudfederico@gmail.com>
2021-10-21 12:37:07 -0300

    cookies 4th try

--- pages/api/auth/callback/idp.js
+++ pages/api/auth/callback/idp.js
@@ -30,10 +30,10 @@ export default function handler(req, res) {
         const id_token = data.id_token || "";
         const access_token = data.access_token || "";
         const refresh_token = data.refresh_token || "";
-        setCookie(res, "idp_id_token", data.id_token, { path: "/" });
-        res.redirect(
-          `/success?id_token=${id_token}&access_token=${access_token}&refresh_token=${refresh_token}`
-        );
+        setCookie(res, "idp_id_token", id_token, { path: "/" });
+        setCookie(res, "idp_id_token", access_token, { path: "/" });
+        setCookie(res, "idp_id_token", refresh_token, { path: "/" });
+        res.redirect(`/success`);
       });
   } catch (error) {
     res.status(500).json(error);
--- pages/success.js
+++ pages/success.js
@@ -1,16 +1,26 @@
 import Head from "next/head";
 import { useEffect, useState } from "react";
 import jwt_decode from "jwt-decode";
+import cookieCutter from "cookie-cutter";
 
 import { useRouter } from "next/dist/client/router";
 
-const Success = ({ id_token, access_token, refresh_token }) => {
+const Success = () => {
   const [decodedData, setDecodedData] = useState(null);
   const router = useRouter();
 
   useEffect(() => {
-    if (id_token) setDecodedData(jwt_decode(id_token));
-  }, [id_token]);
+    const id_token = cookieCutter.get("idp_id_token");
+
+    if (id_token) {
+      console.log("token from cookie", id_token);
+      setDecodedData(jwt_decode(id_token));
+    }
+  }, []);
+
+  // useEffect(() => {
+  //   if (id_token) setDecodedData(jwt_decode(id_token));
+  // }, [id_token]);
 
   useEffect(() => {
     console.log("Decoded JWT: ", decodedData);
@@ -23,7 +33,7 @@ const Success = ({ id_token, access_token, refresh_token }) => {
       "https://idp-nextjs-test.netlify.app/api/auth/signout"
     );
     const url = `https://idpsesiont.telecom.com.ar/openam/oauth2/realms/convergente/connect/endSession?id_token_hint=${id_token}&post_logout_redirect_uri=${redirect_uri}`;
-    
+
     router.push(url);
   };
 
@@ -36,7 +46,7 @@ const Success = ({ id_token, access_token, refresh_token }) => {
 
       <main className="p-12 max-w-6xl flex flex-col">
         <h1 className="text-2xl font-bold pb-4">Success</h1>
-        <p>
+        {/* <p>
           <span className="font-bold">ID Token:</span> {id_token}
         </p>
         <p>
@@ -44,7 +54,7 @@ const Success = ({ id_token, access_token, refresh_token }) => {
         </p>
         <p>
           <span className="font-bold">Refresh Token:</span> {refresh_token}
-        </p>
+        </p> */}
         <p>
           <span className="font-bold">Sub:</span> {decodedData?.sub}
         </p>
@@ -91,5 +101,41 @@ export async function getServerSideProps(context) {
     };
   }
 }
+
+// export async function getServerSideProps(context) {
+//   try {
+//     if (
+//       context.query.id_token &&
+//       context.query.access_token &&
+//       context.query.refresh_token
+//     ) {
+//       const id_token = context.query.id_token;
+//       const access_token = context.query.access_token;
+//       const refresh_token = context.query.refresh_token;
+//       return {
+//         props: {
+//           id_token: id_token,
+//           access_token: access_token,
+//           refresh_token: refresh_token,
+//         },
+//       };
+//     } else {
+//       return {
+//         props: {
+//           id_token: null,
+//           access_token: null,
+//           refresh_token: null,
+//         },
+//       };
+//     }
+//   } catch (error) {
+//     console.error(error);
+//     return {
+//       props: {
+//         err: "not_found",
+//       },
+//     };
+//   }
+// }
 
 export default Success;

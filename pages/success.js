fd04e83311b07e76194899482483af8c4d91a9f2
Fede Varayoud <varayoudfederico@gmail.com>
2021-10-22 11:05:33 -0300

    Add related data decode

--- pages/success.js
+++ pages/success.js
@@ -4,6 +4,9 @@ import jwt_decode from "jwt-decode";
 import cookieCutter from "cookie-cutter";
 import { useRouter } from "next/dist/client/router";
 
+const demoToken =
+  "eyJ0eXAiOiJKV1QiLCJraWQiOiJ3VTNpZklJYUxPVUFSZVJCL0ZHNmVNMVAxUU09IiwiYWxnIjoiUlMyNTYifQ.eyJhdF9oYXNoIjoiMG9TM1o0dENkSEhmaTY4dWRja2NKZyIsInN1YiI6IjExMzAzNTI1NTUiLCJvcmciOiJUZWxlY29tIiwiYXVkaXRUcmFja2luZ0lkIjoiM2NlZjFjMTQtYmE0Ny00MzVlLWI4N2QtMjUwYTVhOTRhN2ExLTEwMzQ2ODciLCJzdWJuYW1lIjoiMTEzMDM1MjU1NSIsImlzcyI6Imh0dHBzOi8vaWRwc2VzaW9udC50ZWxlY29tLmNvbS5hcjo0NDMvb3BlbmFtL29hdXRoMi9jb252ZXJnZW50ZSIsInRva2VuTmFtZSI6ImlkX3Rva2VuIiwibm9uY2UiOiJNYWN5bW1sUmFhclg0c3F3NkJob0hPR3pqTGpqODlWVWo3c0Z1TnZFWVhBIiwic2lkIjoieUdpcmZvZjFKYVA4bGhwYlFldXFENjg4MmR0SWZEN3dHVzUxY3lOdElrcz0iLCJhdWQiOiJvaWRjLXBwZC10ZXN0IiwiY19oYXNoIjoiN3Q4SGZPUlR5RUE0Y2d2RmIxVmpEUSIsImFjciI6IjAiLCJvcmcuZm9yZ2Vyb2NrLm9wZW5pZGNvbm5lY3Qub3BzIjoib2N3Z09VMnlsQmQ2WHhSaWF2QWcwdjZuRjBBIiwic19oYXNoIjoiVVNMNlpSUG91S0VEandzWkdRUG9WdyIsImF6cCI6Im9pZGMtcHBkLXRlc3QiLCJhdXRoX3RpbWUiOjE2MzQ5MDk4NzUsInJlYWxtIjoiL2NvbnZlcmdlbnRlIiwicmVsYXRlZERhdGEiOnt9LCJleHAiOjE2MzQ5MTM0NzYsInRva2VuVHlwZSI6IkpXVFRva2VuIiwiaWF0IjoxNjM0OTA5ODc2fQ.x3ceag0vMnbRJ2NJ3vpB0eb_zjwi2-Lh4UUDYrGteUzDWR97zfFOwWIOa9C-dhRTRfbktt4oPGDkOInmVOWH6RF4lR8WB3gEl6ocjMdvrRPrYJ-iziQ-01YWpjXfVKYnvRIEHXPiizb6krNqLongk3kyP-2UzXAzii0iA4MGM-E8oCNdmpt3QJ5yyRJjqWr3LiofMZgHp2XcBTRJueoO2WXclczDiWA82286-Y40L7Cf6hERJ6zZyy9m-eI1k499qT7PHiYZ4BjpEGv3mIOhgZTdKYelayYZqVPaOUqUGe4yrCJNLqTUW3V6L7zs7jywvMrLREz1Df6hRzsEJDpUKg";
+
 /*
 Tercer paso del flujo de IDP
 Una vez que se obtienen los token de acceso y se guardaron en cookies desde el archivo idp.js, se redirecciona
@@ -20,7 +23,8 @@ const Success = () => {
   const router = useRouter();
 
   useEffect(() => {
-    const id_token = cookieCutter.get("idp_id_token");
+    // const id_token = cookieCutter.get("idp_id_token");
+    const id_token = demoToken;
     const refresh_token = cookieCutter.get("idp_refresh_token");
     const access_token = cookieCutter.get("idp_access_token");
 
@@ -35,17 +39,19 @@ const Success = () => {
 
   useEffect(() => {
     console.log("Decoded JWT: ", decodedData);
+    if (decodedData?.relatedData) {
+      const relatedData = JSON.stringify(decodedData.relatedData);
+      console.log("Related Data:", relatedData);
+    }
   }, [decodedData]);
 
   const logout = () => {
     console.log("Logout...");
-
+    const idToken = cookieCutter.get("idp_id_token") || "";
     const redirect_uri = encodeURI(
       "https://idp-nextjs-test.netlify.app/api/auth/signout"
     );
-    const url = `https://idpsesiont.telecom.com.ar/openam/oauth2/realms/convergente/connect/endSession?id_token_hint=${cookieCutter.get(
-      "idp_id_token"
-    )}&post_logout_redirect_uri=${redirect_uri}`;
+    const url = `https://idpsesiont.telecom.com.ar/openam/oauth2/realms/convergente/connect/endSession?id_token_hint=${idToken}&post_logout_redirect_uri=${redirect_uri}`;
 
     router.push(url);
   };

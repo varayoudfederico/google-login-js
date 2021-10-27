af6aa63130aa12cd9cfe24e0b156a809b7d51d7d
Fede Varayoud <varayoudfederico@gmail.com>
2021-10-27 15:46:09 -0300

    pass id to front test

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -2,6 +2,14 @@ import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
 export default NextAuth({
+  callbacks: {
+    async session({ session, token, user }) {
+      console.log(token);
+      // Send properties to the client, like an access_token from a provider.
+      session.externalId = "external test";
+      return session;
+    },
+  },
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,

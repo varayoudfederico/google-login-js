257165c0b3d18a97fea26692998d1541ca7a437e
Fede Varayoud <varayoudfederico@gmail.com>
2021-10-28 10:49:10 -0300

    manual logout try

--- pages/api/logout.js
+++ pages/api/logout.js
@@ -5,11 +5,11 @@ import { getToken } from "next-auth/jwt";
 const secret = "Fede";
 
 export default async function handler(req, res) {
-  const token = await getToken({ req, secret, encryption: false, raw: true });
+  const token = await getToken({ req, secret });
   console.log("Tokenx   : ", JSON.stringify(token, null, 2));
   const url = `https://idpsesiont.telecom.com.ar/openam/oauth2/realms/convergente/connect/endSession?id_token_hint=${token}&post_logout_redirect_uri=https://idp-nextjs-test2.netlify.app/api/auth/signout`;
-  const response = await fetch(url);
-  const data = await response.json();
+  const { body } = await fetch(url);
+  const data = await body.json();
   console.log("Logout response: ", data);
   res.status(200).json(data);
 }

1e14e4b7e1a7fc78a9f147941ff112ddfeb5e4b9
Fede Varayoud <varayoudfederico@gmail.com>
2021-11-26 12:12:10 -0300

    Remove redirect from logout

--- pages/api/auth/logout.ts
+++ pages/api/auth/logout.ts
@@ -6,7 +6,7 @@ export default async function handler(req, res) {
     secret: process.env.NEXTAUTH_JWT_SECRET,
   });
   // console.log("Got token: ", id_token);
-  const url = `https://idpsesiont.telecom.com.ar/openam/oauth2/realms/convergente/connect/endSession?id_token_hint=${id_token}&post_logout_redirect_uri=https://idp-nextjs-test2.netlify.app/api/auth/signout`;
+  const url = `https://idpsesiont.telecom.com.ar/openam/oauth2/realms/convergente/connect/endSession?id_token_hint=${id_token}`;
   const response = await fetch(url);
   const data = await response.json();
   res.status(200).json(data);
